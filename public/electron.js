699d259919841f0e03e00a25498f8149e7392680
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2019-09-04 17:30:58 +0800

    testing building a production app

--- public/electron.js
+++ public/electron.js
@@ -12,6 +12,21 @@ function createWindow() {
   mainWindow = new BrowserWindow({width: 900, height: 680});
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
+
+  mainWindow.webContents.openDevTools();
+
+  // set menu
+  const Menu = electron.Menu;
+  const MenuItem = electron.MenuItem;
+  const menu = new Menu();
+  menu.append(new MenuItem({
+    label: 'Toggle DevTools',
+    accelerator: 'F12',
+    click: () => {
+      mainWindow.webContents.toggleDevTools();
+    }
+  }));
+  Menu.setApplicationMenu(menu);
 }
 
 app.on('ready', createWindow);
